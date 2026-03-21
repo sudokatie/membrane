85cc56ab9c2970472486edc1d16445a2b993ca86
Katie <everthejetsets@gmail.com>
2026-03-21 16:05:03 -0500

    wire up CLI commands
    - create, start, run, state, kill, delete, list
    - spec command generates default OCI config
    - --root flag for custom state directory
    - tabular list output

--- cmd/membrane/main.go
+++ cmd/membrane/main.go
@@ -9,17 +9,32 @@ import (
 	"encoding/json"
 	"fmt"
 	"os"
+	"text/tabwriter"
 
 	"github.com/spf13/cobra"
+	"github.com/sudokatie/membrane/internal/container"
 )
 
 const version = "0.1.0"
+
+var (
+	stateRoot string
+	force     bool
+)
 
 func main() {
 	if err := rootCmd.Execute(); err != nil {
 		fmt.Fprintln(os.Stderr, err)
 		os.Exit(1)
 	}
+}
+
+func getManager() *container.Manager {
+	config := container.DefaultConfig()
+	if stateRoot != "" {
+		config.StateRoot = stateRoot
+	}
+	return container.NewManager(config)
 }
 
 var rootCmd = &cobra.Command{
@@ -32,7 +47,13 @@ var versionCmd = &cobra.Command{
 	Use:   "version",
 	Short: "Print version information",
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Printf("membrane version %s\n", version)
+		info := map[string]string{
+			"version":    version,
+			"ociVersion": "1.0.2",
+		}
+		enc := json.NewEncoder(os.Stdout)
+		enc.SetIndent("", "  ")
+		enc.Encode(info)
 	},
 }
 
@@ -41,11 +62,12 @@ var createCmd = &cobra.Command{
 	Short: "Create a container",
 	Args:  cobra.ExactArgs(2),
 	RunE: func(cmd *cobra.Command, args []string) error {
-		id := args[0]
-		bundle := args[1]
-		fmt.Printf("Creating container %s from %s\n", id, bundle)
-		// TODO: implement
-		return nil
+		mgr := getManager()
+		_, err := mgr.Create(&container.CreateOptions{
+			ID:     args[0],
+			Bundle: args[1],
+		})
+		return err
 	},
 }
 
@@ -54,10 +76,10 @@ var startCmd = &cobra.Command{
 	Short: "Start a created container",
 	Args:  cobra.ExactArgs(1),
 	RunE: func(cmd *cobra.Command, args []string) error {
-		id := args[0]
-		fmt.Printf("Starting container %s\n", id)
-		// TODO: implement
-		return nil
+		mgr := getManager()
+		return mgr.Start(&container.StartOptions{
+			ID: args[0],
+		})
 	},
 }
 
@@ -66,11 +88,11 @@ var runCmd = &cobra.Command{
 	Short: "Create and start a container",
 	Args:  cobra.ExactArgs(2),
 	RunE: func(cmd *cobra.Command, args []string) error {
-		id := args[0]
-		bundle := args[1]
-		fmt.Printf("Running container %s from %s\n", id, bundle)
-		// TODO: implement
-		return nil
+		mgr := getManager()
+		return mgr.Run(&container.CreateOptions{
+			ID:     args[0],
+			Bundle: args[1],
+		})
 	},
 }
 
@@ -79,18 +101,14 @@ var stateCmd = &cobra.Command{
 	Short: "Query container state",
 	Args:  cobra.ExactArgs(1),
 	RunE: func(cmd *cobra.Command, args []string) error {
-		id := args[0]
-		// TODO: get actual state
-		state := map[string]interface{}{
-			"ociVersion": "1.0.2",
-			"id":         id,
-			"status":     "created",
-			"pid":        0,
-			"bundle":     "",
+		mgr := getManager()
+		st, err := mgr.State(args[0])
+		if err != nil {
+			return err
 		}
 		enc := json.NewEncoder(os.Stdout)
 		enc.SetIndent("", "  ")
-		return enc.Encode(state)
+		return enc.Encode(st)
 	},
 }
 
@@ -99,14 +117,15 @@ var killCmd = &cobra.Command{
 	Short: "Send signal to container",
 	Args:  cobra.RangeArgs(1, 2),
 	RunE: func(cmd *cobra.Command, args []string) error {
-		id := args[0]
 		signal := "SIGTERM"
 		if len(args) > 1 {
 			signal = args[1]
 		}
-		fmt.Printf("Sending %s to container %s\n", signal, id)
-		// TODO: implement
-		return nil
+		mgr := getManager()
+		return mgr.Kill(&container.KillOptions{
+			ID:     args[0],
+			Signal: signal,
+		})
 	},
 }
 
@@ -115,24 +134,63 @@ var deleteCmd = &cobra.Command{
 	Short: "Delete a container",
 	Args:  cobra.ExactArgs(1),
 	RunE: func(cmd *cobra.Command, args []string) error {
-		id := args[0]
-		fmt.Printf("Deleting container %s\n", id)
-		// TODO: implement
-		return nil
+		mgr := getManager()
+		return mgr.Delete(args[0], force)
 	},
 }
 
 var listCmd = &cobra.Command{
 	Use:   "list",
 	Short: "List containers",
+	Aliases: []string{"ls"},
 	RunE: func(cmd *cobra.Command, args []string) error {
-		fmt.Println("ID\tSTATUS\tPID\tBUNDLE")
-		// TODO: implement
-		return nil
+		mgr := getManager()
+		containers, err := mgr.List()
+		if err != nil {
+			return err
+		}
+
+		w := tabwriter.NewWriter(os.Stdout, 0, 0, 3, ' ', 0)
+		fmt.Fprintln(w, "ID\tSTATUS\tPID\tBUNDLE")
+		for _, c := range containers {
+			fmt.Fprintf(w, "%s\t%s\t%d\t%s\n",
+				c.ID, c.State.Status, c.State.Pid, c.Bundle)
+		}
+		return w.Flush()
+	},
+}
+
+var specCmd = &cobra.Command{
+	Use:   "spec",
+	Short: "Generate a default OCI spec",
+	RunE: func(cmd *cobra.Command, args []string) error {
+		// Output default spec
+		spec := map[string]interface{}{
+			"ociVersion": "1.0.2",
+			"root": map[string]interface{}{
+				"path":     "rootfs",
+				"readonly": false,
+			},
+			"process": map[string]interface{}{
+				"terminal": false,
+				"user":     map[string]int{"uid": 0, "gid": 0},
+				"args":     []string{"/bin/sh"},
+				"env":      []string{"PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin", "TERM=xterm"},
+				"cwd":      "/",
+			},
+			"hostname": "container",
+		}
+		enc := json.NewEncoder(os.Stdout)
+		enc.SetIndent("", "  ")
+		return enc.Encode(spec)
 	},
 }
 
 func init() {
+	rootCmd.PersistentFlags().StringVar(&stateRoot, "root", "", "state directory (default: /run/membrane)")
+
+	deleteCmd.Flags().BoolVarP(&force, "force", "f", false, "force delete running container")
+
 	rootCmd.AddCommand(versionCmd)
 	rootCmd.AddCommand(createCmd)
 	rootCmd.AddCommand(startCmd)
@@ -141,4 +199,5 @@ func init() {
 	rootCmd.AddCommand(killCmd)
 	rootCmd.AddCommand(deleteCmd)
 	rootCmd.AddCommand(listCmd)
+	rootCmd.AddCommand(specCmd)
 }
